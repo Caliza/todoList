e451181be00c2232f0cb563fe5f69e712d043867
Caliza <rances.rodriguez@gmail.com>
2024-07-08 18:10:03 -0700

    redacted TodoList and TodoItem

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,24 +1,19 @@
 import { TodoItem } from "./TodoItem"
 
-export function Todolist({todos, toggleTodo, deleteTodo}) {
-    return (
-        <ul className="list">
-            {todos.length === 0 && "No Todos"}
-            {todos.map(todo => {
-                return (
-                    <TodoItem 
-                    {...todo}
-                    //id={todo.id} 
-                    //completed={todo.completed} 
-                    //title={todo.title} 
-                    key={todo.id}
-                    toggleTodo={toggleTodo}
-                    deleteTodo={deleteTodo}
-                    />
-                    
-                )
-            })}
-
-        </ul>
-    )
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul className="list">
+      {todos.length === 0 && "No Todos"}
+      {todos.map(todo => {
+        return (
+          <TodoItem
+            {...todo}
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        )
+      })}
+    </ul>
+  )
 }
